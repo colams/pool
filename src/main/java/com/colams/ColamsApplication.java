d23b1523e80ee12334309a93e81dc9e23b7d88fd
zhangzt <zhangzt@ctrip.com>
2018-02-06 09:56:54 +0800

    spring boot init

---
+++ src/main/java/com/colams/ColamsApplication.java
@@ -0,0 +1,16 @@
+package com.colams;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColamsApplication {
+
+    // private final static Logger logger = LoggerFactory.getLogger(ColasApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColamsApplication.class, args);
+    }
+}
