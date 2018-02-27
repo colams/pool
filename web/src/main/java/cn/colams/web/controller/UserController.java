62220d67216af8f4824895e30f6651ebab9ec4f2
zhangzt <zhangzt@ctrip.com>
2018-02-27 14:18:34 +0800

    update datachange_lasttime

--- web/src/main/java/cn/colams/web/controller/UserController.java
+++ web/src/main/java/cn/colams/web/controller/UserController.java
@@ -4,14 +4,11 @@ import cn.colams.common.utils.UuidUtils;
 import cn.colams.model.dto.Acount;
 import cn.colams.model.dto.BaseDTO;
 import cn.colams.web.utils.ResultUtils;
-import com.alibaba.dubbo.common.utils.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
@@ -30,26 +27,5 @@ public class UserController {
         session.setAttribute(uuid, acount);
 
         return ResultUtils.createResult(acount);
-    }
-
-
-    @GetMapping("/uuid1")
-    public String getUUID1(HttpSession session) {
-        String uuid = UuidUtils.getUuid();
-        List<String> uuidList = null;
-
-        Object obj = session.getAttribute("uuid");
-
-        if (obj instanceof ArrayList) {
-            uuidList = (ArrayList) obj;
-        }
-
-        if (CollectionUtils.isEmpty(uuidList)) {
-            uuidList = new ArrayList<String>();
-        }
-
-        uuidList.add(uuid);
-        session.setAttribute("uuid", uuidList);
-        return uuid;
     }
 }
