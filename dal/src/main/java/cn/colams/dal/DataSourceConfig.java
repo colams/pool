59f009633dcfc431dc1a7cb1878b3d9ad74974d4
zhangzt <zhangzt@ctrip.com>
2018-07-05 16:51:48 +0800

    new version

--- dal/src/main/java/cn/colams/dal/DataSourceConfig.java
+++ dal/src/main/java/cn/colams/dal/DataSourceConfig.java
@@ -1,16 +1,17 @@
 package cn.colams.dal;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
 
 import javax.sql.DataSource;
 
@@ -20,9 +21,16 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = "cn.colams.dal", sqlSessionFactoryRef = "sqlSessionFactoryBean")
 @Configuration
 public class DataSourceConfig {
+
+
+
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.test")
     public DataSource dataSource() throws Exception {
+
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        // comboPooledDataSource.set
+
         return DataSourceBuilder.create().build();
     }
 
