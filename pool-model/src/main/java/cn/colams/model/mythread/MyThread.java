ab9f7bef345d519d237e74eb001858267136a047
zhangzt <zhangzt@ctrip.com>
2020-04-18 19:38:12 +0800

    opt 优化

---
+++ pool-model/src/main/java/cn/colams/model/mythread/MyThread.java
@@ -0,0 +1,10 @@
+package cn.colams.model.mythread;
+
+public class MyThread extends Thread{
+
+    @Override
+    public void run(){
+        super.run();
+        System.out.println("My Thread");
+    }
+}
