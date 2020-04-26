ce305d3d3f5461ede846cb675241d9b45989f88d
zzt <zhangzt@ctrip.com>
2020-04-26 19:29:24 +0800

    update

--- pool-web/src/test/java/cn/colams/javatest/EnterMain.java
+++ pool-web/src/test/java/cn/colams/javatest/EnterMain.java
@@ -1,4 +1,10 @@
 package cn.colams.javatest;
 
 public class EnterMain {
+
+    public static void main(String[] args) {
+
+        InitOrder.staticMethod();
+        System.out.println(InitOrder.staticString);
+    }
 }
