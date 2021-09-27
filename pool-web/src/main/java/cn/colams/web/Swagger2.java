7d1288b85593acd9eaa6e1c7727a70f6d59d1523
cc <1423177715@qq.com>
2021-09-28 00:16:32 +0800

    spring boot swagger

--- pool-web/src/main/java/cn/colams/web/Swagger2.java
+++ pool-web/src/main/java/cn/colams/web/Swagger2.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,19 +16,24 @@ public class Swagger2 {
     public Docket createRestApi() {
         // http://localhost:5000/swagger-ui.html
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+                .pathMapping("/")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("cn.colams.web.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                        .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                        .termsOfServiceUrl("http://blog.didispace.com/")
+                        .version("1.0")
+                        .build());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .version("1.0")
-                .build();
-    }
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+//                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+//                .termsOfServiceUrl("http://blog.didispace.com/")
+//                .version("1.0")
+//                .build();
+//    }
 }
--- pool-web/src/main/java/cn/colams/web/WebConfigurer.java
+++ pool-web/src/main/java/cn/colams/web/WebConfigurer.java
@@ -2,7 +2,10 @@ package cn.colams.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by zhangzt on 2018/6/1.
@@ -12,10 +15,15 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/templates/");
-        registry.addResourceHandler("/*.html").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
-        registry.addResourceHandler("/assets/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/assets/");
+//        registry.addResourceHandler("/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/templates/");
+//        registry.addResourceHandler("/*.html").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+//        registry.addResourceHandler("/assets/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/assets/");
         registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
         super.addResourceHandlers(registry);
     }
 
