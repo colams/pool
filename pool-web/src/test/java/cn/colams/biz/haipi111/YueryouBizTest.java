dfac4014dc729a0985fb49037fff7f04b5032050
cc <1423177715@qq.com>
2022-10-12 11:47:28 +0800

    记录本地日志

--- pool-web/src/test/java/cn/colams/biz/haipi111/YueryouBizTest.java
+++ pool-web/src/test/java/cn/colams/biz/haipi111/YueryouBizTest.java
@@ -2,6 +2,7 @@ package cn.colams.biz.haipi111;
 
 
 import cn.colams.biz.dalTest;
+import cn.colams.common.utils.JacksonSerializerUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -24,7 +25,35 @@ public class YueryouBizTest {
 
     @Test
     public void testPostYueryou() {
-        String result = yueryouBiz.postYueryou("123456");
-        logger.info(result);
+        char[] intArray = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char[] arrayArray = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        char[] charArray = new char[6];
+
+        for (int i = 6613; i <= 999999; i++) {
+            String tem = String.valueOf(i);
+            char[] tempChar = tem.toCharArray();
+            int n = charArray.length - tempChar.length;
+            if (n > 0) {
+                for (int j = 0; j < n; j++) {
+                    charArray[j] = '0';
+                }
+
+                for (int j = 0; j < tempChar.length; j++) {
+                    charArray[n + j] = tempChar[j];
+                }
+
+                logger.info(JacksonSerializerUtil.serialize(charArray));
+            } else {
+                charArray = tempChar;
+            }
+
+            String pwd = String.copyValueOf(charArray);
+            String result = yueryouBiz.postYueryou(pwd);
+            logger.info(result);
+            if (result.equalsIgnoreCase("success")) {
+                break;
+            }
+        }
     }
 }
